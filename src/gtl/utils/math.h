642e32722cd1d180cbe1e94abfd6cf6fc4c05695
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-10-01 13:53:27 +0200

    added constant expression template bitwidth<>

--- src/gtl/utils/math.h
+++ src/gtl/utils/math.h
@@ -7,6 +7,7 @@ namespace gtl {
 namespace utils {
 namespace math {
 
+// Computes the value of A raised to the power B
 template <size_t A, size_t B>
 struct pow
 {
@@ -17,6 +18,19 @@ template <size_t A>
 struct pow<A, 0>
 {
     static const size_t value = 1;
+};
+
+// Retruns number of bits required to store value A
+template <size_t A>
+struct bitwidth
+{
+    static const size_t value = 1 + bitwidth<(A >> 1)>::value;
+};
+
+template <>
+struct bitwidth<0>
+{
+    static const size_t value = 0;
 };
 
 } // namespace math
